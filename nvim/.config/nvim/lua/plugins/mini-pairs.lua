2da3a3616ed44315ae47f4b77c05874683ba327f
Le Van Dong <itlvd@outlook.com>
2026-06-25 15:56:29 +0700

    feat: update Kitty configuration to disable XIM for improved input handling; add new plugins for snacks and undotree

---
+++ nvim/.config/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-mini/mini.pairs",
+    -- config = function(_, opts)
+    --   LazyVim.mini.pairs(opts)
+    --   -- Remove smart Enter/Backspace so pressing Enter or BS inside a pair
+    --   -- behaves like a normal keypress instead of expanding/deleting the pair.
+    --   pcall(vim.keymap.del, "i", "<CR>")
+    --   pcall(vim.keymap.del, "i", "<BS>")
+    -- end,
+  },
+}
