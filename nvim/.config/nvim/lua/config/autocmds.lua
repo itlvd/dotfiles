39fc7795cd8426c8bdf105b759ec34072999b52c
Le Van Dong <itlvd@outlook.com>
2026-06-27 10:36:41 +0700

    fix: remove duplicate IME setting and update autocmds for Kitty terminal

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,6 @@
--- autocmds are set here
+-- Tắt Kitty Keyboard Protocol (CSI u) để tránh lỗi double Enter / Backspace trong Kitty terminal
+vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function()
+    io.stdout:write("\027[<u")
+  end,
+})
